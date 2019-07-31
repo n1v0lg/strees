372c76b45d42b718295259c98191df2434848b52
n1v0lg <n1v0lg@users.noreply.github.com>
2019-07-31 13:57:18 +0200

    WIP naive compute best gini

--- strees.py
+++ strees.py
@@ -5,6 +5,8 @@ import library as mpc_lib
 import os
 import sys
 
+ONE_CINT = cint(1)
+
 
 def print_list(lst):
     print_str("[ ")
@@ -60,7 +62,7 @@ def get_samples():
     samples_mat = [
         [0, 10, 0],
         [1, 12, 0],
-        [1, 3, 0]
+        [1, 30, 1]
     ]
     sec_mat = input_matrix(samples_mat)
     return Samples(samples_mat, 1, 1)
@@ -78,11 +80,71 @@ def get_comparison_mat(elements):
     return mat
 
 
+def prod(left, right):
+    return [l * r for l, r in zip(left, right)]
+
+
+def inner_prod(left, right):
+    return sum(prod(left, right))
+
+
+def bit_not(bits):
+    return [ONE_CINT - b for b in bits]
+
+
+def best_gini_idx(samples, comp_mats, active_samples):
+    def thresh_helper(category_vec, active_samples, comp_row):
+        above_threshold_cat = inner_prod(
+            comp_row, prod(active_samples, category_vec))
+        above_threshold = inner_prod(comp_row, active_samples)
+        return (above_threshold_cat, above_threshold)
+
+    N = len(samples.samples)
+    category_vec_1 = samples.get_col(N - 1)
+    category_vec_0 = bit_not(category_vec_1)
+
+    numerators = []
+    denoms = []
+    # TODO isn't this matrix multiplication?
+    for k in range(N):
+        for i in range(samples.m):
+            comp_row = comp_mats[i][k]
+            comp_row_not = bit_not(comp_row)
+
+            above_threshold_cat_1, above_threshold = \
+                thresh_helper(category_vec_1, active_samples, comp_row)
+
+            below_threshold_cat_1, below_threshold = \
+                thresh_helper(category_vec_1, active_samples, comp_row_not)
+
+            above_threshold_cat_0 = inner_prod(
+                comp_row, prod(active_samples, category_vec_0))
+
+            below_threshold_cat_0 = inner_prod(
+                comp_row_not, prod(active_samples, category_vec_0))
+
+            # TODO check if squaring is properly supported
+            numerator = (above_threshold_cat_1**2) * below_threshold + \
+                (below_threshold_cat_1**2) * above_threshold
+
+            numerator += (above_threshold_cat_0**2) * below_threshold + \
+                (below_threshold_cat_0**2) * above_threshold
+            numerators.append(numerator)
+
+            denom = above_threshold * below_threshold
+            denoms.append(denom)
+
+    print_list(numerators)
+    print_list(denoms)
+
+
 def stree(samples):
     comp_mats = dict()
     for cont_attr in range(samples.m):
         comp_mats[cont_attr] = get_comparison_mat(samples.get_col(cont_attr))
-        print_mat(comp_mats[cont_attr])
+    active_samples = [ONE_CINT for _ in samples.samples]
+    best_gini_idx(samples, comp_mats, active_samples)
+
 
 def main():
     stree(get_samples())
