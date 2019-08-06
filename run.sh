5d18fbf8239c2aa6b0e26c9bdc0fdd0a1a29c366
n1v0lg <n1v0lg@users.noreply.github.com>
2019-08-06 13:59:29 +0200

    Project restructure

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-MPC_SRC_NAME=main.py
+MPC_SRC_NAME=c45.py
 
 HERE=$(cd `dirname $0`; pwd)
 SPDZ_ROOT=${HERE}/../MP-SPDZ
 cd ${SPDZ_ROOT}
 
 # .py extension so we can use an IDE
-./compile.py ${HERE}/${MPC_SRC_NAME}
+./compile.py ${HERE}/c45/${MPC_SRC_NAME}
 ./Scripts/mascot.sh ${MPC_SRC_NAME}
