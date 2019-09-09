22ace0ce638e579902f11eedcef126746a383575
n1v0lg <n1v0lg@users.noreply.github.com>
2019-09-09 14:31:45 +0200

    Parse timing info

--- benchmarks/micro/run.sh
+++ benchmarks/micro/run.sh
@@ -1,10 +1,23 @@
 #!/usr/bin/env bash
 
+parse_compile_time() {
+    # TODO this is hacky
+    echo "$1" | grep "^real"
+}
+
+parse_exec_time() {
+    # TODO this is hacky
+    echo "$1" | grep "^real"
+    echo "$1" | grep "^Time"
+    echo "$1" | grep "^Data sent"
+}
+
 MPC_SRC_NAME=run.py
+COMPILE_OPTS="--ring=64 --optimize-hard --insecure"
 
 HERE=$(cd `dirname $0`; pwd)
 SPDZ_ROOT=${HERE}/../../../MP-SPDZ
 cd ${SPDZ_ROOT}
 
-(time ./compile.py --ring=64 --optimize-hard --insecure ${HERE}/${MPC_SRC_NAME}) 2>&1 | (grep real)
-(time ./Scripts/ring.sh ${MPC_SRC_NAME}) 2>&1 | grep real
+parse_compile_time "$({ time ./compile.py ${COMPILE_OPTS} ${HERE}/${MPC_SRC_NAME}; } 2>&1)"
+parse_exec_time "$({ time ./Scripts/ring.sh ${MPC_SRC_NAME}; } 2>&1)"
